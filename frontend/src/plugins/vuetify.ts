061833f9e108162af41aec79b1eac72ab7f20531
Josh Willox <joshwillox@gmail.com>
2022-03-11 00:28:11 +1100

    Add vuetify to frontend

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,11 @@
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Vuetify
+import { createVuetify } from "vuetify";
+import { loadFonts } from "./webfontloader";
+
+loadFonts();
+
+export default createVuetify();
