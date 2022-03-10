030e448fb7d51a10d51633a9c7ca660b90191508
Josh Willox <joshwillox@gmail.com>
2022-03-10 20:56:02 +1100

    Make backend work as python module

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -14,7 +13,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
