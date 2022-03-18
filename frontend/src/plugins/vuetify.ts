f975f07846fc28f1aefbd5588577185dba343d40
Josh Willox <joshwillox@gmail.com>
2022-03-18 21:18:11 +1100

    Update theme colors

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -7,4 +7,27 @@ import { loadFonts } from "./webfontloader";
 
 loadFonts();
 
-export default createVuetify();
+export default createVuetify({
+  theme: {
+    variations: {
+      colors: ["primary", "secondary"],
+      lighten: 1,
+      darken: 2
+    },
+    themes: {
+      light: {
+        colors: {
+          primary: "#2196F3",
+          secondary: "#E91E63"
+        }
+      },
+      dark: {
+        dark: true,
+        colors: {
+          primary: "#212121",
+          secondary: "#E91E63"
+        }
+      }
+    }
+  }
+});
