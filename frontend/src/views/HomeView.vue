3b5f12b050a95357e5b847fd860223b9ef7f09f7
Josh Willox <joshwillox@gmail.com>
2022-03-20 00:30:36 +1100

    Add demo video and frame/timecode counter to HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,66 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
-import { RouterLink } from "vue-router";
+import type { VideoJsPlayer } from "video.js";
+import { computed } from "vue";
+import VideoJS from "../components/VideoJS.vue";
+
+let player = $ref<InstanceType<typeof VideoJS> | null>(null);
+
+const timecode = computed(() => player?.timecode || 0);
+const frame = computed({
+  get: () => player?.frame || 0,
+  set: value => {
+    player?.seekTo(Number(value));
+  }
+});
+
+const playerReady = (videojs: VideoJsPlayer) => {
+  console.log("player-ready", videojs);
+};
+
+const toggleVideo = () => {
+  if (player?.videojs?.paused()) {
+    player.videojs.play();
+  } else {
+    player?.videojs?.pause();
+  }
+};
 </script>
 
 <template>
-  <main>
-    To <RouterLink to="/about">About</RouterLink>
-    <TheWelcome />
-  </main>
+  <v-container fluid class="pa-0 fill-height">
+    <div class="d-flex pa-2">
+      <v-text-field
+        label="Frame"
+        variant="outlined"
+        density="compact"
+        v-model="frame"
+        hide-details
+        persistent-placeholder
+      ></v-text-field>
+      <v-text-field
+        class="pl-2"
+        label="Timecode"
+        variant="outlined"
+        density="compact"
+        v-model="timecode"
+        readonly
+        hide-details
+        persistent-placeholder
+      ></v-text-field>
+    </div>
+
+    <VideoJS
+      ref="player"
+      :fps="23.976"
+      @player-ready="playerReady"
+      @keydown.space="toggleVideo"
+      @keydown.left="() => player?.seekBackward()"
+      @keydown.right="() => player?.seekForward()"
+    >
+      <source
+        src="https://daiz.github.io/frame-accurate-ish/time.mp4"
+        type="video/mp4"
+      />
+    </VideoJS>
+  </v-container>
 </template>
