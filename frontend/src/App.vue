e9652928792e67a3377cea65da39892b364a47ea
Josh Willox <joshwillox@gmail.com>
2022-03-19 23:38:56 +1100

    Add dark mode support and tab layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,94 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import {
+  usePreferredColorScheme,
+  useStorage,
+  type BasicColorSchema
+} from "@vueuse/core";
+import { computed, ref, watch } from "vue";
+import { useRoute } from "vue-router";
+import SettingsMenu from "./components/SettingsMenu.vue";
+
+const showSettings = ref(false);
+
+// compute which theme to use
+const themeMode = useStorage<BasicColorSchema>("theme", "auto");
+const systemTheme = usePreferredColorScheme();
+const theme = computed(() =>
+  themeMode.value == "auto" ? systemTheme.value : themeMode.value
+);
+
+// handle tracking active tab
+const route = useRoute();
+const tab = ref(window.location.pathname.substring(1) || "extract");
+const items = ["extract", "label", "analyse"];
+
+watch(route, () => {
+  if (route.name == "home") {
+    tab.value = "extract";
+  } else {
+    tab.value = route.name as string;
+  }
+});
 </script>
 
 <template>
-  <v-app>
-    <v-app-bar color="#1867c0" density="compact" class="text-white" flat app>
+  <v-app :theme="theme">
+    <SettingsMenu v-model="showSettings" v-model:theme="themeMode" />
+
+    <v-system-bar app fixed color="primary-darken-1">
+      <v-icon class="pr-3">mdi-movie</v-icon>
+      <span>Hello_World.mp4</span>
+      <v-spacer />
+    </v-system-bar>
+
+    <v-app-bar color="primary" density="compact" flat app>
       <template v-slot:prepend>
-        <v-app-bar-nav-icon />
+        <v-tabs v-model="tab">
+          <v-tab
+            v-for="item in items"
+            :key="item"
+            :value="item"
+            :to="'/' + item"
+          >
+            {{ item }}
+          </v-tab>
+        </v-tabs>
       </template>
-      <v-app-bar-title tag="div">DLC</v-app-bar-title>
+
       <template v-slot:append>
-        <v-btn icon="mdi-dots-vertical"></v-btn>
+        <v-btn icon="mdi-cog" @click.stop="showSettings = true"></v-btn>
       </template>
     </v-app-bar>
 
     <v-main>
-      <v-system-bar>
-        <v-icon class="pr-3">mdi-movie</v-icon>
-        <span>Hello_World.mp4</span>
-        <v-spacer />
-      </v-system-bar>
-      <RouterView />
+      <v-container class="pa-0" fluid>
+        <router-view v-slot="{ Component }">
+          <transition>
+            <keep-alive>
+              <component :is="Component" />
+            </keep-alive>
+          </transition>
+        </router-view>
+      </v-container>
     </v-main>
   </v-app>
 </template>
+
+<style>
+html {
+  overflow-y: auto !important;
+}
+.v-toolbar[app] {
+  margin-top: 24px !important;
+}
+.v-system-bar[app] {
+  top: 0px;
+  z-index: 1005;
+}
+.v-main {
+  margin-top: 24px;
+}
+.v-application {
+  min-height: 100vh;
+}
+</style>
