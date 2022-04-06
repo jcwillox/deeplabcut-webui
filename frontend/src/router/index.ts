ed1a611af958b6d24c817bc8cb398639678dcb3d
Josh Willox <joshwillox@gmail.com>
2022-04-06 23:49:01 +1000

    Refactor extract view into its own component

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import AboutView from "../views/AboutView.vue";
-import HomeView from "../views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import ExtractView from "@/views/ExtractView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +8,14 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView
+      redirect: () => {
+        return { name: "extract" };
+      }
     },
     {
       path: "/extract",
       name: "extract",
-      component: HomeView
+      component: ExtractView
     },
     {
       path: "/label",
