b470cca2697cdbfa5c02d4ff09729c5351a33292
Josh Willox <joshwillox@gmail.com>
2022-04-19 22:56:01 +1000

    Add clean scripts

---
+++ scripts/clean_backend.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+from glob import glob
+
+os.chdir(os.path.dirname(os.path.dirname(__file__)))
+
+shutil.rmtree("backend/.pytest_cache/", ignore_errors=True)
+
+for path in glob("backend/*.egg-info"):
+    shutil.rmtree(path, ignore_errors=True)
+
+for path in glob("backend/**/__pycache__/", recursive=True):
+    shutil.rmtree(path, ignore_errors=True)
