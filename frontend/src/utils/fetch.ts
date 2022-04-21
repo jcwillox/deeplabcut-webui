3550359170d60611f356c1c92c1d324acf62ef86
Josh Willox <joshwillox@gmail.com>
2022-04-21 17:38:20 +1000

    Add createUrl method and useUrl hook

--- frontend/src/utils/fetch.ts
+++ frontend/src/utils/fetch.ts
@@ -1,15 +1,41 @@
 import { useStore } from "@/stores/global";
-import { createFetch } from "@vueuse/core";
+import { createFetch, type MaybeRef } from "@vueuse/core";
+import { readonly, ref, unref, watch } from "vue";
+
+export const useUrl = (...parts: MaybeRef<string>[]) => {
+  const url = ref("");
+  const store = useStore();
+  watch(
+    [
+      () => store.backend,
+      () => store.project,
+      ...parts.filter(part => typeof part != "string")
+    ],
+    () => {
+      // we skip update if there is no project selected
+      // to avoid setting the url to a broken link
+      if (store.project) {
+        url.value = createUrl(...parts.map(part => unref(part)));
+      }
+    },
+    { immediate: true }
+  );
+  return readonly(url);
+};
+
+export const createUrl = (...parts: string[]) => {
+  const store = useStore();
+  const url = new URL(parts.join("/"), store.backend);
+  if (store.project != "") {
+    url.searchParams.append("project", store.project);
+  }
+  return url.toString();
+};
 
 export const useFetch = createFetch({
   options: {
     async beforeFetch({ url, options }) {
-      const store = useStore();
-      const newUrl = new URL(url, store.backend);
-      if (store.project != "")
-        newUrl.searchParams.append("project", store.project);
-      url = newUrl.toString();
-      return { url, options };
+      return { url: createUrl(url), options };
     }
   },
   fetchOptions: {
