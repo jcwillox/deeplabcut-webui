5528a71562eed779b1bb741773c0170b3d17cf17
kelkon <75928644+kelkon@users.noreply.github.com>
2022-05-03 23:14:19 +1000

    Created the LabelView

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,8 @@
-import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
 import vuetify from "./plugins/vuetify";
+import router from "./router";
 
 const app = createApp(App);
 
