5bff88984fd49419d060526426a7499d43120489
kelkon <75928644+kelkon@users.noreply.github.com>
2022-05-03 23:37:02 +1000

    cleaned up code

--- frontend/src/views/LabelView.vue
+++ frontend/src/views/LabelView.vue
@@ -6,7 +6,6 @@ import { computed, ref, watch, watchEffect } from "vue";
 import type { VCarouselItem } from "vuetify/components";
 
 const store = useStore();
-
 const imgIndex = ref(0);
 const carouselEl = ref<InstanceType<typeof VCarouselItem>[] | null>(null);
 const url = ref("");
@@ -23,7 +22,7 @@ watch(
 
 const framesUrl = computed(() => url.value + "/frames");
 
-const { data: frames, execute: refetchFrames } = useFetch(framesUrl, {
+const { data: frames } = useFetch(framesUrl, {
   refetch: true
 })
   .get()
@@ -37,58 +36,36 @@ watchEffect(() => {
       instance.dispose();
     }
     instance = panzoom(carouselEl.value[imgIndex.value].$el);
-    //console.log(carouselEl.value[imgIndex.value].$el.firstElementChild);
   }
 });
-/*
-const carouselChangeHandler = (index: number) => {
-  console.log(`index ${index} `);
-  if (carouselEl.value) {
-    if (instance) {
-      instance.dispose();
-    }
-    instance = panzoom(carouselEl.value[index].$el);
-    //console.log(carouselEl.value[imgIndex.value].$el.firstElementChild);
-  }
-};
-*/
 </script>
 
 <template>
-  <div id="labeller" style="display: flex">
-    <div id="main" style="border-style: solid; width: 80%; height: 1000px">
-      <div id="carousel" style="border-style: dotted">
-        <v-carousel
-          hide-delimiters
-          progress="primary"
-          v-if="frames"
-          class="fill-height"
-          v-model="imgIndex"
-        >
-          <v-carousel-item
-            v-for="(frame, i) in frames"
-            :key="i"
-            ref="carouselEl"
-          >
-            <v-img :src="createUrl(framesUrl, frame)">
-              <template v-slot:placeholder>
-                <v-row class="fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular
-                    indeterminate
-                    color="grey lighten-5"
-                  ></v-progress-circular>
-                </v-row>
-              </template>
-            </v-img>
-          </v-carousel-item>
-        </v-carousel>
-      </div>
+  <v-container fluid class="d-flex pa-1">
+    <div style="flex-grow: 4">
+      <v-carousel
+        v-if="frames"
+        hide-delimiters
+        progress="primary"
+        class="fill-height"
+        v-model="imgIndex"
+      >
+        <v-carousel-item v-for="(frame, i) in frames" :key="i" ref="carouselEl">
+          <v-img :src="createUrl(framesUrl, frame)">
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </v-carousel-item>
+      </v-carousel>
     </div>
-
-    <div id="sidebar" style="border-style: solid; width: 20%">
-      <div id="miniview" style="border-style: solid">
-        <v-img :src="createUrl(framesUrl, frames[imgIndex])" :eager="true" />
-      </div>
+    <div style="flex-grow: 1" class="ml-1">
+      <v-img :src="createUrl(framesUrl, frames[imgIndex])" :eager="true" />
     </div>
-  </div>
+  </v-container>
 </template>
